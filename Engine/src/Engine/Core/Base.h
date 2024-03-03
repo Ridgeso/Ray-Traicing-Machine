a5cb4fff72d0eb3ec21751e711a8f478cf0a66d7
Ridgeso <nienekpalo@gmail.com>
2024-03-03 12:04:25 +0100

    Add Share alias for pointers

--- Engine/src/Engine/Core/Base.h
+++ Engine/src/Engine/Core/Base.h
@@ -8,9 +8,18 @@ namespace RT
 	using Local = std::unique_ptr<T>;
 
 	template <typename T, typename... Args>
-	inline constexpr auto makeLocal(Args&&... args)
+	inline constexpr decltype(auto) makeLocal(Args&&... args)
 	{
-		return std::make_unique<decltype(T(args...))>(std::forward<Args>(args)...);
+		return std::make_unique<decltype(T{args...})>(std::forward<Args>(args)...);
+	}
+	
+	template <typename T>
+	using Share = std::shared_ptr<T>;
+
+	template <typename T, typename... Args>
+	inline constexpr decltype(auto) makeShare(Args&&... args)
+	{
+		return std::make_shared<decltype(T{args...}) > (std::forward<Args>(args)...);
 	}
 
 }
